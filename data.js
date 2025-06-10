b063d571435088508e8a92cb1dbbda44bbc15bb0
Pitossomo <pedro.h.teles@hotmail.com>
2025-06-10 06:48:49 -0300

    feat: add data.js file with design patterns definitions

---
+++ data.js
@@ -0,0 +1,27 @@
+const DESIGN_PATTERNS = [
+  {
+    name: "Singleton",
+    description: "A abstração que representa a classe como um objeto único",
+    icon: "fa-sun"
+  },
+  {
+    name: "Factory",
+    description: "Uma abstração que representa a criação de objetos sem especificar sua classe concreta",
+    icon: "fa-industry"
+  },
+  {
+    name: "Observer",
+    description: "Uma abstração que permite a notificação de mudanças em objetos",
+    icon: "fa-eye"
+  },
+  {
+    name: "Strategy",
+    description: "Uma abstração que permite a escolha de algoritmos em execução",
+    icon: "fa-chess-board"
+  },
+  {
+    name: "Template Method",
+    description: "Uma abstração que define um método comum para suas subclasses",
+    icon: "fa-clone"
+  }
+];

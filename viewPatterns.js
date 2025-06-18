a283ad007e3bbd939a4627346345b206bd78e8d6
Pitossomo <pedro.h.teles@hotmail.com>
2025-06-17 22:30:06 -0300

    feat: refactor viewPatterns.js to improve pattern rendering and organization

--- viewPatterns.js
+++ viewPatterns.js
@@ -1,13 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const patternsContainer = document.querySelector(".patterns-container");
-  const patternTemplate = document.getElementById("pattern-template");
 
-  DESIGN_PATTERNS.forEach((designPattern) => {
-    const patternClone = patternTemplate.content.cloneNode(true);
-    patternClone.querySelector("h2").textContent = designPattern.name;
-    patternClone.querySelector("p").textContent = designPattern.description;
-    patternClone.querySelector(".fa-icon").classList.add(designPattern.icon);
-
-    patternsContainer.appendChild(patternClone);
+  const container = document.querySelector(".patterns-container");
+  DESIGN_PATTERNS.forEach(group => {
+    const groupSection = createGroup(group.name, group.description)
+    container.appendChild(groupSection);
+    populate(groupSection, group.patterns);
   });
 });
+
+function createGroup(name, description) {
+  const groupSection = document.createElement("section");
+  groupSection.classList.add("pattern-group");
+  groupSection.innerHTML = `
+      <h2>${name}</h2>
+      <p>${description}</p>
+      <div class="patterns"></div>
+  `;
+  return groupSection
+}
+
+function populate(section, patterns) {
+  console.log(section);
+  const groupDiv = section.querySelector(".patterns");
+  
+  patterns.forEach(({name, description, icon}) => {
+    const patternDiv = document.createElement("div");
+    patternDiv.classList.add("pattern");
+    patternDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${description}</p>
+        <i class="fa-solid ${icon} fa-5x "></i>
+    `;
+    groupDiv.appendChild(patternDiv);
+  });
+}

214e4f9446c7e95007c9fff04da1a76494ab1a95
Pitossomo <pedro.h.teles@hotmail.com>
2025-06-23 15:10:05 -0300

    feat/examples: Add Bridge & Composite code example

--- examples.js
+++ examples.js
@@ -563,5 +563,245 @@ console.log('');
 console.log('Client: Mas posso operá-la via Adapter:');
 const adapter = new Adapter(adaptee);
 clientCode(adapter);
+`, 
+  "Bridge": `
+/**
+ * A Abstraction define a parte de controle da hierarquia de duas classes.
+ * Ela mantém uma referência para um objeto Implementation e delega
+ * todo o trabalho real para esse objeto.
+ */
+class Abstraction {
+   protected implementation: Implementation;
+
+   constructor(implementation: Implementation) {
+     this.implementation = implementation;
+   }
+
+   public operation(): string {
+     const result = this.implementation.operationImplementation();
+     return "Abstraction: Operação básica com :\\n" + result;
+   }
+}
+
+/**
+ * Podemos estender a Abstracion sem mudar a classe Implementation.
+ */
+class ExtendedAbstraction extends Abstraction {
+   public operation(): string {
+     const result = this.implementation.operationImplementation();
+     return "ExtendedAbstraction: Operação estendida com:\\n" + result;
+   }
+}
+
+/**
+ * A Implementation define a interface para todas as classes de implementações.
+ * A interface não precisa ser idêntica à interface Abstraction, podendo ser
+ * completamente diferentes. Tipicamente, a interface Implementation provê
+ * apenas operações primitivas, enquanto a Abstraction define operações de 
+ * maior nivel baseados nessas primitivas.
+ */
+interface Implementation {
+   operationImplementation(): string;
+}
+
+/**
+ * Cada classe concreta de implementação corresponde a uma plataforma específica
+ * e implementa a interfacec Implementation usando a API da sua plataforma.
+ */
+class ConcreteImplementationA implements Implementation {
+   public operationImplementation(): string {
+     return 'ConcreteImplementationA: O resultado da plataforma A é este.';
+   }
+}
+
+class ConcreteImplementationB implements Implementation {
+   public operationImplementation(): string {
+     return 'ConcreteImplementationB: O resultado da plataforma B é este.';
+   }
+}
+
+/**
+ * Exceto pela fase de inicialização quando um objeto Abstraction é ligado a um 
+ * objeto especifico de Implementação, o código do cliente depende apenas da
+ * classe Abstraction. Assim, o código do cliente pode suportar qualquer 
+ * combinação de abstração-implementação.
+ */
+function clientCode(abstraction: Abstraction) {
+   // ..
+
+   console.log(abstraction.operation());
+
+   // ..
+}
+
+/**
+ * O código do cliente deve ser capaz de operar com qualquer combinação 
+ * pré-configurada de Abstraction-Implementation.
+ */
+let implementation = new ConcreteImplementationA();
+let abstraction = new Abstraction(implementation);
+clientCode(abstraction);
+
+console.log('');
+
+implementation = new ConcreteImplementationB();
+abstraction = new ExtendedAbstraction(implementation);
+clientCode(abstraction);
+ `,
+  "Composite": `
+/**
+ * A classe base Component declara operações comuns para os objetos de uma 
+ * composição, sejam simples ou complexos.
+ */
+abstract class Component {
+  protected parent!: Component | null;
+
+  /**
+   * Opcionalmente, o Component pode declarar uma interface de setter e getter
+   * para o componente pai em uma estrutura de árvore, bem como prover uma
+   * implementação padrão para esses métodos
+   */
+  public setParent(parent: Component | null) {
+    this.parent = parent;
+  }
+
+  public getParent(): Component | null {
+    return this.parent;
+  }
+
+  /**
+   * Em alguns casos, pode ser benéfico definir operações de gestão dos filhos
+   * diretamente na classe base Component. Desa forma, não precisamos expor
+   * classes concretas de componentes para o código cliente, mesmo durante a
+   * montagem da árvore de objetos. Por outro lado, esses métodos serão vazios 
+   * para os componentes Leaf, nas pontas da árvore.
+   */
+  public add(component: Component): void { }
+
+  public remove(component: Component): void { }
+
+  /**
+   * Podemos prover um método que mostra ao cliente se um componente pode
+   * ou não comportar filhos.
+   */
+  public isComposite(): boolean {
+    return false;
+  }
+
+  /**
+   * O Component base pode implementar algum comportamento padrão ou deixá-lo
+   * para as classes concretas (declarando o método como "abstract").
+   */
+  public abstract operation(): string;
+}
+
+/**
+ * A classe Leaf representa um objeto na ponta de uma composição, ou seja, have
+ * que não pode ter nenhum filho.
+ *
+ * Em geral, o objeto Leaf é que faz o trabalho real, enquanto o objeto Composite
+ * apenas delega chamada para seus sub-componentes. 
+ */
+class Leaf extends Component {
+  public operation(): string {
+    return 'Leaf';
+  }
+}
+
+/**
+ * A classe Composite representa o objeto complexo que pode ter filhos. 
+ * Em geral, o Composite delega o trabalho a seus filhos e agrega o resultado.
+ */
+class Composite extends Component {
+  protected children: Component[] = [];
+
+  /**
+   * Um objeto Composite pode adicionar ou remover outros componentes (simples
+   * ou complexos) de sua lista de filhos.
+   */
+  public add(component: Component): void {
+    this.children.push(component);
+    component.setParent(this);
+  }
+
+  public remove(component: Component): void {
+    const componentIndex = this.children.indexOf(component);
+    this.children.splice(componentIndex, 1);
+
+    component.setParent(null);
+  }
+
+  public isComposite(): boolean {
+    return true;
+  }
+
+  /**
+   * O Composite executa sua lógica primária de um modo específico, 
+   * percorrendo resursivamente por seus filhos, coletando e agregando os
+   * resultados. Como os filhos passam as chamadas para seus próprios filhos
+   * e assim por diante, toda a árvore é percorrida como resultado.
+   */
+  public operation(): string {
+    const results:string[] = [];
+    for (const child of this.children) {
+      results.push(child.operation());
+    }
+
+    return "Galho("+results.join('+');
+  }
+}
+
+/**
+ * o código cliente opera com todos os componentes via interface base.
+ */
+function clientCode(component: Component) {
+  // ...
+
+  console.log("RESULT: " + component.operation());
+
+  // ...
+}
+
+/**
+ * Assim, o código cliente pode operar com componentes Leaf simples...
+ */
+const simple = new Leaf();
+console.log('Cliente: Tenho um componente simples:');
+clientCode(simple);
+console.log('');
+
+/**
+ * ...bem como objectos complexos Composite.
+ */
+const tree = new Composite();
+const branch1 = new Composite();
+branch1.add(new Leaf());
+branch1.add(new Leaf());
+const branch2 = new Composite();
+branch2.add(new Leaf());
+tree.add(branch1);
+tree.add(branch2);
+console.log('Cliente: Tenho uma árvore complexa:');
+clientCode(tree);
+console.log('');
+
+/**
+ * Graças às operações de gestão dos filhos do Component base, o código cliente
+ * pode operar com qualquer componente, simples ou complexo, independente de sua
+ * classe concreta.
+ */
+function clientCode2(component1: Component, component2: Component) {
+  // ...
+
+  if (component1.isComposite()) {
+    component1.add(component2);
+  }
+  console.log("RESULTADO: " + component1.operation());
+
+  // ...
+}
+
+console.log('Cliente: Não preciso saber as classes mesmo ao gerir a árvore:');
+clientCode2(tree, simple);
 `,
 }

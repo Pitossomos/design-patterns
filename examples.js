a0606a83cbcb40019927b86b1a09a0a10108a4d1
Pitossomo <pedro.h.teles@hotmail.com>
2025-06-24 06:07:31 -0300

    feat/examples: Add Facade & Decorator code examples

--- examples.js
+++ examples.js
@@ -804,4 +804,193 @@ function clientCode2(component1: Component, component2: Component) {
 console.log('Cliente: Não preciso saber as classes mesmo ao gerir a árvore:');
 clientCode2(tree, simple);
 `,
+  "Decorator": `
+/**
+ * A interface base Component define operações que podem ser alteradas por
+ * Decorators.
+ */
+interface Component {
+    operation(): string;
+}
+
+/**
+ * As classes concretas de Components implementam os padrões das operações. 
+ * Podem haver várias variações dessas classes.
+ */
+class ConcreteComponent implements Component {
+    public operation(): string {
+        return 'ConcreteComponent';
+    }
+}
+
+/**
+ * A classe base Decorator segue a mesma interface Component dos demais.
+ * Seu propósito principal é definir a interface que envolverá todos os 
+ * Decorators concretos. O Decorator padrão pode incluir o campo para 
+ * armazenar o Component envolvido e os meios para inicializá-lo.
+ */
+class Decorator implements Component {
+    protected component: Component;
+
+    constructor(component: Component) {
+        this.component = component;
+    }
+
+    /**
+     * O Decorator delega todo o trabalho para o componente envolvido.
+     */
+    public operation(): string {
+        return this.component.operation();
+    }
+}
+
+/**
+ * Os Decorators concretos chamam o objeto envolvido alteram seu resultado.
+ */
+class ConcreteDecoratorA extends Decorator {
+    /**
+     * Decorators concretos podem chamar a implementação herdada da operação,
+     * ao invés de chamar o objeto envolvido diretamente. Essa abordagem
+     * simplifica a extensão da classe Decorator.
+     */
+    public operation(): string {
+        return "ConcreteDecoratorA("+super.operation();
+    }
+}
+
+/**
+ * Decorators concretos podem executar seu comportamento antes ou depois da
+ * chamada do objeto envolvido.
+ */
+class ConcreteDecoratorB extends Decorator {
+    public operation(): string {
+        return "ConcreteDecoratorB("+super.operation();
+    }
+}
+
+/**
+ * O código cliente opera com todos os objetos usando a interface Component.
+ * Assim, ele se torna independente da classe concreta do objeto.
+ */
+function clientCode(component: Component) {
+    // ...
+
+    console.log("RESULT: "+component.operation());
+
+    // ...
+}
+
+/**
+ * O código cliente pode operar com componentes simples...
+ */
+const simple = new ConcreteComponent();
+console.log('Cliente: Tenho um componente simples:');
+clientCode(simple);
+console.log('');
+
+/**
+ * ...e com componentes decorados.
+ *
+ * Veja que Decorators podem envolver componentes ou mesmo outros decorators.
+ */
+const decorator1 = new ConcreteDecoratorA(simple);
+const decorator2 = new ConcreteDecoratorB(decorator1);
+console.log('Cliente: Agora eu tenho um componente decorado.');
+clientCode(decorator2);
+`,
+  "Facade": `
+/**
+ * A classe Facade provê uma interface simples para a lógica complexa de 
+ * vários subsistemas. Ela delega as requisições do cliente para os objetos
+ * apropriados dentro do subsistema. Também é responsável por gerir seu
+ * ciclo de vida. Tudo isso protege o cliente contra a complexidade 
+ * indesejadada dos subsistemas.
+ */
+class Facade {
+  protected subsystem1: Subsystem1;
+
+  protected subsystem2: Subsystem2;
+
+  /**
+   * Dependendo da necessidade da aplicação, a Facade pode ser provida 
+   * com os subsistemas ou criá-los por conta própria.
+   */
+  constructor(subsystem1?: Subsystem1, subsystem2?: Subsystem2) {
+  this.subsystem1 = subsystem1 || new Subsystem1();
+  this.subsystem2 = subsystem2 || new Subsystem2();
+  }
+
+  /**
+   * Os métodos da Facade são atalhos convenientes para as funcionalidade 
+   * sofisticadas dos subsistemas. No entanto, os clientes só têm acesso
+   * a uma fração das capacidades de um subsistema.
+   */
+  public operation(): string {
+  let result = "Facade inicializando subsistemas:\\n";
+  result += this.subsystem1.operation1();
+  result += this.subsystem2.operation1();
+  result += "Facade ordenando subsistemas para performar a ação:\\n";
+  result += this.subsystem1.operationN();
+  result += this.subsystem2.operationZ();
+
+  return result;
+  }
+}
+
+/**
+ * Os subsistemas podem aceitar requisições da Facade ou diretamente do cliente.
+ * Em todo caso. para o Subsystem, a Facade é apenas outro cliente, e não uma
+ * parte do Subsystem.
+ */
+class Subsystem1 {
+  public operation1(): string {
+  return "Subsystem1: Pronto!\\n";
+  }
+
+  // ...
+
+  public operationN(): string {
+  return "Subsystem1: Vai!\\n";
+  }
+}
+
+/**
+ * Algumas facades podem operar com múltiplos subsistemas ao mesmo tempo.
+ */
+class Subsystem2 {
+  public operation1(): string {
+  return "Subsystem21: Pronto!\\n";
+  }
+
+  // ...
+
+  public operationZ(): string {
+  return 'Subsystem2: Fogo!';
+  }
+}
+
+/**
+ * O código do cliente opera com subsistemas complexos através da interface
+ * simples que a Facade provê. Quando a Facade gere o ciclo de vida do 
+ * subsistema, o cliente pode nem mesmo conhecer a existência do subsistema.
+ * Essa abordagem mantém a complexidade sob controle.
+ */
+function clientCode(facade: Facade) {
+  // ...
+
+  console.log(facade.operation());
+
+  // ...
+}
+
+/**
+ * O código cliente pode ter algum subsistema já criado. Nesse caso, pode 
+ * fazer sentido inicializar a Facade com esses objetos ao invés de deixar
+ * a Facade criar novas instâncias.
+ */
+const subsystem1 = new Subsystem1();
+const subsystem2 = new Subsystem2();
+const facade = new Facade(subsystem1, subsystem2);
+clientCode(facade);
+`,
 }

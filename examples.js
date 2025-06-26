8993c23773bc70ba666c830078be2c29c9194752
Pitossomo <pedro.h.teles@hotmail.com>
2025-06-26 06:53:14 -0300

    feat/examples: Add Proxy & Chain of Responsibility code examples

--- examples.js
+++ examples.js
@@ -1098,6 +1098,198 @@ addCarToPoliceDatabase(factory, 'CL234IR', 'James Doe', 'BMW', 'X1', 'vermelho')
 factory.listFlyweights();
 `,
   "Proxy": `
+/**
+ * A interface Subject declara operações comuns para o RealSubject e o Proxy.
+ * Enquanto o cliente trabalha com o RealSubject usando essa interface,
+ * você poderá passar um proxy no lugar de um sujeito real.
+ */
+interface Subject {
+  request(): void;
+}
+
+/**
+ * O RealSubject contém a lógica central do negócio e, usualmente, 
+ * realiza algum trabelho útil, que pode ser lento ou sensível, p.ex.,
+ * corrigir dados de entrada. Um Proxy pode resolver esses problemas
+ * sem mudanças no código do RealSubject.
+ */
+class RealSubject implements Subject {
+  public request(): void {
+    console.log('RealSubject: Manipulando a requisição.');
+  }
+}
+
+/**
+ * A classe Proxy implementa a mesma interface do RealSubject.
+ */
+class Proxy implements Subject {
+  private realSubject: RealSubject;
+
+  /**
+   * A Proxy mantém a referência a um objeto do tipo RealSubject.
+   * Ela pode ser passada pelo cliente ou carregada por lazy loading.
+   */
+  constructor(realSubject: RealSubject) {
+    this.realSubject = realSubject;
+  }
+
+  /**
+   * As aplicações mais comuns do padrão Proxy envolvem: lazy loading, cache,
+   * controle de acesso, logs, etc. Um Proxy pode realizar uma dessas tarefas
+   * e, dependendo do resultado, passar a execução para o mesmo método de seu
+   * RealSubject associado.
+   */
+  public request(): void {
+    if (this.checkAccess()) {
+      this.realSubject.request();
+      this.logAccess();
+    }
+  }
+
+  private checkAccess(): boolean {
+    // Algumas verificações são feitas.
+    console.log('Proxy: Verificando acesso antes do disparo da requisição.');
+
+    return true;
+  }
+
+  private logAccess(): void {
+    console.log('Proxy: Preparando os logs do acesso.');
+  }
+}
+
+/**
+ * Espera-se que código do cliente opere com todos os objetos (tanto RealSubject
+ * quanto Proxy) via interface Subject. Na vida real, no entanto, os clientes 
+ * operam quase sempre com os RealSubjects diretamente. Nesse caso, para 
+ * implementar o padrão mais facilmente, você pode estender o Proxy a partir 
+ * da classe RealSubject.
+ */
+function clientCode(subject: Subject) {
+  // ...
+
+  subject.request();
+
+  // ...
+}
+
+console.log('Cliente: Executando o código cliente com um RealSubject:');
+const realSubject = new RealSubject();
+clientCode(realSubject);
+
+console.log('');
+
+console.log('Cliente: Executando o mesmo código cliente com uma Proxy:');
+const proxy = new Proxy(realSubject);
+clientCode(proxy);
+`,
+  "Chain of Responsibility": `
+/**
+ * A interface Handler declara um método para construir uma sequência de 
+ * Handlers. Declara também um método para executar uma requisição.
+ */
+interface Handler<Request = string | null, Result = string | null> {
+  setNext(handler: Handler<Request, Result>): Handler<Request, Result>;
+
+  handle(request: Request): Result;
+}
+
+/**
+ * A sequência pode ser implementada em de uma classe base AbstractHandler.
+ */
+abstract class AbstractHandler implements Handler
+{
+  private nextHandler: Handler;
+
+  public setNext(handler: Handler): Handler {
+    this.nextHandler = handler;
+    // Retorna um handler aqui permite associações convenientes no formato:
+    // monkey.setNext(squirrel).setNext(dog);
+    return handler;
+  }
+
+  public handle(request: string | null): string | null {
+    if (this.nextHandler) {
+      return this.nextHandler.handle(request);
+    }
+
+    return null;
+  }
+}
+
+/**
+ * Todos os Handlers concretos manipulam uma requisição ou a repassam para
+ * para o próximo Handler na sequência.
+ */
+class MonkeyHandler extends AbstractHandler {
+  public handle(request: string | null): string | null{
+    if (request === 'Banana') {
+      return "Macaco: Vou comer " + request;
+    }
+    return super.handle(request);
+
+  }
+}
+
+class SquirrelHandler extends AbstractHandler {
+  public handle(request: string | null): string | null{
+    if (request === 'Noz') {
+      return "Esquilo: Vou comer " + request;
+    }
+    return super.handle(request);
+  }
+}
+
+class DogHandler extends AbstractHandler {
+  public handle(request: string | null): string | null{
+    if (request === 'Carne') {
+      return "Cão: Vou comer " + request;
+    }
+    return super.handle(request);
+  }
+}
+
+/**
+ * O código do cliente em geral consegue operar com um único Handler.
+ * Na maioria dos casos, ele não está ciente de que o Handler faz parte
+ * de uma sequência. 
+ */
+function clientCode(handler: Handler) {
+  const foods = ['Noz', 'Banana', 'Xícara de café'];
+
+  for (const food of foods) {
+    console.log("Cliente: Quem quer " + food);
+
+    const result = handler.handle(food);
+    if (result) {
+      console.log("  " + result);
+    } else {
+      console.log("  " + food + " não foi consumida.");
+    }
+  }
+}
+
+/**
+ * A outra parte do código do cliente constrói a verdadeira sequência.
+ */
+const monkey = new MonkeyHandler();
+const squirrel = new SquirrelHandler();
+const dog = new DogHandler();
+
+monkey.setNext(squirrel).setNext(dog);
+
+/**
+ * The client should be able to send a request to any handler, not just the
+ * first one in the chain.
+ */
+console.log("Sequência: Macaco > Esquilo > Cão\\n");
+clientCode(monkey);
+console.log('');
+
+console.log('Subsequência: Esquilo > Cão\\n');
+clientCode(squirrel);
+`,
+  "Mediator": `
 TODO
 `
 }
